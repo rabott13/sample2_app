0fa4867b23c51f9e5cc9f0144e698dca248e4780
rabott13 <rabott13@gmail.com>
2014-01-17 03:23:45 -0600

    full title help

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "StaticPages" do
   		expect(pages).to have_content('Sample App')
     end
 
-    it "should have the title 'home'" do
+    it "should have the base title 'home'" do
     	visit '/static_pages/home'
     	expect(pages).to have_title ("Ruby on Rails Tutorial Sample App | Home")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
 
   end
