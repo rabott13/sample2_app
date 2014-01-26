a4ddc08d9a40cb7a17a3cad86bc0834873882272
rabott13 <rabott13@gmail.com>
2014-01-26 05:02:17 -0600

    finsihed microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,13 +12,29 @@ describe "StaticPages" do
   end
 
 
-describe "Help page" do	
+  describe "Help page" do	
 	before { visit help_path}
 
     it {should have_content ('Help')}
     it {should have_title (full_title ('Help' ))}
-   end		
+  end		
 
+  describe "for signed-in users" do
+    let(:user) {FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
+  end
+   
+  it "should render the user's feed" do
+    user.feed.each do |item|
+      expect(page).to have_selector("li##{item.id}", text: item.content)
+    end
+  end        
+ end
+end  
 
 
 describe "About page" do
@@ -34,5 +50,7 @@ describe "About page" do
     it {should have_content ('Contact')}
     it {should have_title (full_title ('Contact' ))}
    end  
+
+
 end
 
