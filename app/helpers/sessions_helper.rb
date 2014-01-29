5e093db87ada62e1c477b4d261b311b08e978d6d
rabott13 <rabott13@gmail.com>
2014-01-28 23:55:18 -0600

    update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,10 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
-    current_user = user
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
   end
 
   def signed_in?
@@ -23,8 +25,9 @@ module SessionsHelper
   end
 
   def sign_out
-    current_user = nil
+    current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
+    self.current_user = nil
   end
 
   def redirect_back_or(default)
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,8 +7,8 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
   end
 
   describe "signin" do
@@ -17,18 +17,23 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
 
-      it { should have_selector('title', text: 'Sign in') }
-      it { should have_error_message }
+      it { should have_tile('Sign in') }
+      it { should have_selector('div.alert.alert-error')
 
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_error_message }
+        it { should_not have_selector('div.alert.alert-error') }
       end
     end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { sign_in user }
+      before do
+        fill_in "Email", with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end  
+
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
